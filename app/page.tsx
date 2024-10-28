9a9600f72ab8d6eb0d3d519d41d1879954c09453
Ivan <ivan.se.agalakov@gmail.com>
2024-10-28 17:29:50 -0600

    initial nextjs setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-black text-center flex h-screen justify-center items-center font-bold text-5xl">
+      Bus Kiosk
+    </main>
+  );
+}
