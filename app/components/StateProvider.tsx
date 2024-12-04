10319a910220433943ee51d58e46c26272a7e14a
Ivan <ivan.se.agalakov@gmail.com>
2024-12-04 11:35:25 -0700

    added timer

--- app/components/StateProvider.tsx
+++ app/components/StateProvider.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { createContext, ReactNode, useContext, useState } from "react";
+import { useRouter } from "next/navigation";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
 
 type GlobalStateType = {
   language: string;
@@ -27,6 +28,36 @@ export const GlobalStateProvider = ({ children }: { children: ReactNode }) => {
   const [total, setTotal] = useState<number>(0);
   const [adultAmount, setAdultAmount] = useState<number>(0);
   const [youthAmount, setYouthAmount] = useState<number>(0);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    let idleTimer: NodeJS.Timeout;
+
+    const resetTimer = () => {
+      if (idleTimer) clearTimeout(idleTimer);
+      idleTimer = setTimeout(() => {
+        router.push("/");
+      }, 10000);
+    };
+
+    const handleUserActivity = () => {
+      resetTimer();
+    };
+
+    document.addEventListener("click", handleUserActivity);
+    document.addEventListener("mousemove", handleUserActivity);
+    document.addEventListener("keydown", handleUserActivity);
+
+    resetTimer();
+
+    return () => {
+      clearTimeout(idleTimer);
+      document.removeEventListener("click", handleUserActivity);
+      document.removeEventListener("mousemove", handleUserActivity);
+      document.removeEventListener("keydown", handleUserActivity);
+    };
+  }, [router]);
 
   return (
     <GlobalStateContext.Provider value={{ language, setLanguage, selectedPass, setSelectedPass, total, setTotal, adultAmount, setAdultAmount, youthAmount, setYouthAmount}}>
