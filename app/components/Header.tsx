ae11a2a6887b4e66f8a405d5db7c67c3e88c2eb6
Ivan <ivan.se.agalakov@gmail.com>
2024-12-07 14:30:14 -0700

    changed header styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps {
 export default function Header({ text, language, svg }: HeaderProps) {
   return (
     <div className="absolute z-10 top-4">
-      <div className="bus-button text-3xl flex flex-row items-center space-x-3 bg-gray-500 rounded-none text-black outline-1 hover:bg-gray-500">
+      <div className="text-2xl flex flex-row items-center space-x-3 text-black underline">
         {svg && <div className="svg-container">{svg}</div>}
         <p>{translate(text, language)}</p>
       </div>
