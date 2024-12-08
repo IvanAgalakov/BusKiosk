c6f8eb6e08200a19e8536d1d119afac369ade713
madelineraine <69781611+madelineraine@users.noreply.github.com>
2024-12-07 18:11:00 -0700

    one more

--- app/data/destinations.ts
+++ app/data/destinations.ts
@@ -9,13 +9,13 @@ export interface Destinations {
         value:"chinookmall",
         label: "Chinook Mall",
         routes: [["301 → 42" , "/mapa-esquematico-santiago-1200x584.jpg"], 
-            ["2 → 9" , "/flower.jpg"]]
+            ["2 → 9" , "/mapa-esquematico-santiago-1200x584.jpg"]]
     },
     {
         value: "glenbowmuseum",
         label: "Glenbow Museum",
         routes: [["24 → 52" , "/mapa-esquematico-santiago-1200x584.jpg"], 
-            ["2 → 9" , "/flower.jpg"]]           
+            ["2 → 9" , "/mapa-esquematico-santiago-1200x584.jpg"]]           
     }
   ];
 
